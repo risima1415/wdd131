ab0a6d7376827f3e24d14c5c48be84553c13847b
risima1415 <risima.t.maluleke15@gmail.com>
2026-04-04 00:42:38 +0200

    exploring

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -24,7 +24,7 @@ const temples = [
     dedicated: "2005, August, 7",
     area: 11500,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg" 
   },
   {
     templeName: "Manti Utah",
